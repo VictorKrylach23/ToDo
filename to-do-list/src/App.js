967f9e5973dc8f653639c3ce75576d3090e74737
Victor Krylach <vkrya2023@gmail.com>
2023-09-23 14:04:38 -0400

    Add Loader

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -3,30 +3,39 @@ import { useEffect, useState } from 'react';
 
 function App() {
 	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
+		setIsLoading(true);
+
 		fetch('https://jsonplaceholder.typicode.com/todos')
 			.then((loadedData) => loadedData.json())
 			.then((loadedProducts) => {
 				setProducts(loadedProducts);
-			});
+			})
+			.finally(() => setIsLoading(false));
 	}, []);
 
 	return (
 		<>
 			<div>
 				<div className="toDo">ToDoList</div>
+
 				<ol className="list">
-					{products.map(({ id, title, completed }) => (
-						<li key={id}>
-							{title}
-							<input
-								type="checkbox"
-								className="isComplitedCheckbox"
-								checked={completed}
-							></input>
-						</li>
-					))}
+					{isLoading ? (
+						<div className="loader"></div>
+					) : (
+						products.map(({ id, title, completed }) => (
+							<li key={id}>
+								{title}
+								<input
+									type="checkbox"
+									className="isComplitedCheckbox"
+									checked={completed}
+								></input>
+							</li>
+						))
+					)}
 				</ol>
 			</div>
 		</>
